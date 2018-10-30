eb5a45ef03e0e689f40ef6e70910f63ce95e1256
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-10-30 23:16:59 +0100

    Migrating to fluent-api

--- src/main/java/io/kmruiz/vlingo/bank/Bootstrap.java
+++ src/main/java/io/kmruiz/vlingo/bank/Bootstrap.java
@@ -1,35 +1,42 @@
 package io.kmruiz.vlingo.bank;
 
 
+import io.kmruiz.vlingo.bank.infrastructure.customer.CustomerResource;
+import io.kmruiz.vlingo.bank.infrastructure.transfer.TransferResource;
 import io.vlingo.actors.World;
+import io.vlingo.http.resource.Configuration;
+import io.vlingo.http.resource.Resources;
 import io.vlingo.http.resource.Server;
 
 public class Bootstrap {
-    private static Bootstrap instance;
+    private static final int PORT = 8080;
 
-    public static final Bootstrap instance() {
-        if (instance == null) instance = new Bootstrap();
-        return instance;
-    }
-
-    public final Server server;
-    public final World world;
-
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
         System.out.println("=======================");
         System.out.println("bank: started.");
         System.out.println("=======================");
 
-        Bootstrap.instance();
-    }
+        final var world = World.startWithDefaults("bank");
 
-    private Bootstrap() {
-        this.world = World.startWithDefaults("bank");
-        this.server = Server.startWith(world.stage());
+        final var customerResource = new CustomerResource(world);
+        final var transferResource = new TransferResource(world);
+
+        final var resources = Resources.are(
+                CustomerResource.resourcesOf(customerResource),
+                TransferResource.resourcesOf(transferResource)
+        );
+
+        final var server = Server.startWith(
+                world.stage(),
+                resources,
+                PORT,
+                Configuration.Sizing.define(),
+                Configuration.Timing.define()
+        );
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (instance != null) {
-                instance.server.stop();
+            if (server != null) {
+                server.stop();
 
                 System.out.println("\n");
                 System.out.println("=======================");
@@ -40,7 +47,7 @@ public class Bootstrap {
         }));
     }
 
-    private void pause() {
+    private static void pause() {
         try {
             Thread.sleep(1000L);
         } catch (Exception e) {
--- src/main/java/io/kmruiz/vlingo/bank/infrastructure/customer/CustomerResource.java
+++ src/main/java/io/kmruiz/vlingo/bank/infrastructure/customer/CustomerResource.java
@@ -10,14 +10,55 @@ import io.kmruiz.vlingo.bank.infrastructure.customer.request.DepositRequest;
 import io.kmruiz.vlingo.bank.infrastructure.customer.request.OpenAccountRequest;
 import io.kmruiz.vlingo.bank.infrastructure.customer.request.WithdrawRequest;
 import io.vlingo.actors.Definition;
+import io.vlingo.actors.Stage;
+import io.vlingo.actors.World;
+import io.vlingo.common.Completes;
 import io.vlingo.common.Failure;
 import io.vlingo.http.Response;
-import io.vlingo.http.resource.ResourceHandler;
+import io.vlingo.http.resource.Resource;
 
+import static io.vlingo.http.resource.ResourceBuilder.*;
 import static io.vlingo.http.resource.serialization.JsonSerialization.serialized;
 
-public class CustomerResource extends ResourceHandler {
-    public void newCustomer() {
+public class CustomerResource {
+    private final World world;
+
+    public CustomerResource(final World world) {
+        this.world = world;
+    }
+
+    public static Resource resourcesOf(final CustomerResource resource) {
+        final int threadPool = 10;
+
+        return resource("Customers", threadPool,
+                post("/customers")
+                        .handle(resource::newCustomer),
+                post("/customers/{customerId}/accounts")
+                        .param(String.class)
+                        .body(OpenAccountRequest.class)
+                        .handle(resource::openAccountForCustomer),
+                get("/customers/{customerId}/accounts/{accountId}")
+                        .param(String.class)
+                        .param(String.class)
+                        .handle(resource::getAccountBalanceForCustomer),
+                put("/customers/{customerId}/accounts/{accountId}")
+                        .param(String.class)
+                        .param(String.class)
+                        .body(DepositRequest.class)
+                        .handle(resource::depositIntoAccount),
+                put("/customers/{customerId}/accounts/{accountId}/withdraw")
+                        .param(String.class)
+                        .param(String.class)
+                        .body(WithdrawRequest.class)
+                        .handle(resource::withdrawFromAccount),
+                delete("/customers/{customerId}/accounts/{accountId}")
+                        .param(String.class)
+                        .param(String.class)
+                        .handle(resource::closeAccount)
+        );
+    }
+
+    public Completes<Response> newCustomer() {
         var id = CustomerId.forNewCustomer();
         stage().actorFor(
                 Definition.has(CustomerActor.class, Definition.parameters(id)),
@@ -25,71 +66,75 @@ public class CustomerResource extends ResourceHandler {
                 Customer.addressOf(stage(), id)
         );
 
-        completes().with(Response.of(Response.Status.Created, serialized(id)));
+        return Completes.withSuccess(Response.of(Response.Status.Created, serialized(id)));
     }
 
-    public void openAccountForCustomer(final String customerId, final OpenAccountRequest openAccount) {
-        Customer.find(stage(), CustomerId.of(customerId))
+    public Completes<Response> openAccountForCustomer(final String customerId, final OpenAccountRequest openAccount) {
+        return Customer.find(stage(), CustomerId.of(customerId))
                 .andThenInto(customer -> customer.openAccount(AccountName.of(openAccount.getAccountName())))
-                .andThenConsume(maybeAccount -> completes().with(maybeAccount.resolve(
+                .andThenInto(maybeAccount -> Completes.withSuccess(maybeAccount.resolve(
                         ex -> Response.of(Response.Status.BadRequest, serialized(ex)),
                         accountId -> Response.of(Response.Status.Created, serialized(accountId))
                 )));
     }
 
-    public void getAccountBalanceForCustomer(final String customerId, final String accountId) {
+    public Completes<Response> getAccountBalanceForCustomer(final String customerId, final String accountId) {
         var validCustomerId = CustomerId.of(customerId);
         var validAccountId = AccountId.of(accountId);
 
-        Customer.find(stage(), validCustomerId)
+        return Customer.find(stage(), validCustomerId)
                 .andThenInto(customer -> customer.getAccount(validAccountId))
                 .andThenInto(account -> account.get().balance())
                 .recoverFrom(ex -> Failure.of(new IllegalArgumentException(ex)))
-                .andThenConsume(maybeBalance -> completes().with(maybeBalance.resolve(
+                .andThenInto(maybeBalance -> Completes.withSuccess(maybeBalance.resolve(
                         ex -> Response.of(Response.Status.BadRequest, serialized(ex)),
                         balance -> Response.of(Response.Status.Ok, serialized(balance))
                 )));
     }
 
-    public void depositIntoAccount(final String customerId, final String accountId, final DepositRequest deposit) {
+    public Completes<Response> depositIntoAccount(final String customerId, final String accountId, final DepositRequest deposit) {
         var validCustomerId = CustomerId.of(customerId);
         var validAccountId = AccountId.of(accountId);
 
-        Customer.find(stage(), validCustomerId)
+        return Customer.find(stage(), validCustomerId)
                 .andThenInto(customer -> customer.getAccount(validAccountId))
                 .andThenInto(account -> account.get().deposit(AccountAmount.of(deposit.getAmount())))
                 .recoverFrom(ex -> Failure.of(new IllegalArgumentException(ex)))
-                .andThenConsume(maybeBalance -> completes().with(maybeBalance.resolve(
+                .andThenInto(maybeBalance -> Completes.withSuccess(maybeBalance.resolve(
                         ex -> Response.of(Response.Status.BadRequest, serialized(ex)),
                         balance -> Response.of(Response.Status.Ok, serialized(balance))
                 )));
     }
 
-    public void withdrawFromAccount(final String customerId, final String accountId, final WithdrawRequest withdraw) {
+    public Completes<Response> withdrawFromAccount(final String customerId, final String accountId, final WithdrawRequest withdraw) {
         var validCustomerId = CustomerId.of(customerId);
         var validAccountId = AccountId.of(accountId);
 
-        Customer.find(stage(), validCustomerId)
+        return Customer.find(stage(), validCustomerId)
                 .andThenInto(customer -> customer.getAccount(validAccountId))
                 .andThenInto(account -> account.get().withdraw(AccountAmount.of(withdraw.getAmount())))
                 .recoverFrom(ex -> Failure.of(new IllegalArgumentException(ex)))
-                .andThenConsume(maybeBalance -> completes().with(maybeBalance.resolve(
+                .andThenInto(maybeBalance -> Completes.withSuccess(maybeBalance.resolve(
                         ex -> Response.of(Response.Status.BadRequest, serialized(ex)),
                         balance -> Response.of(Response.Status.Ok, serialized(balance))
                 )));
     }
 
-    public void closeAccount(final String customerId, final String accountId) {
+    public Completes<Response> closeAccount(final String customerId, final String accountId) {
         var validCustomerId = CustomerId.of(customerId);
         var validAccountId = AccountId.of(accountId);
 
-        Customer.find(stage(), validCustomerId)
+        return Customer.find(stage(), validCustomerId)
                 .andThenInto(customer -> customer.getAccount(validAccountId))
                 .andThenInto(account -> account.get().close())
                 .recoverFrom(ex -> Failure.of(new IllegalArgumentException(ex)))
-                .andThenConsume(maybeId -> completes().with(maybeId.resolve(
+                .andThenInto(maybeId -> Completes.withSuccess(maybeId.resolve(
                         ex -> Response.of(Response.Status.BadRequest, serialized(ex)),
                         id -> Response.of(Response.Status.Ok, serialized(id))
                 )));
+    }
+
+    private Stage stage() {
+        return world.stage();
     }
 }
